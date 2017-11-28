7f4940fc0ed54d1e7b3fca822e504b08526710b1
Ivan <ivan.peric93100@gmail.com>
2017-11-28 12:54:11 +0100

    Mise en place du projet, de tous les assets et du visuel global

---
+++ Assets/PlayerRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour {
+
+    public Transform CamTransform;
+    public float SpeedRotation;
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Mouse Y");
+        float y = Input.GetAxis("Mouse X");
+
+        transform.eulerAngles += new Vector3(0, y, 0);
+        CamTransform.eulerAngles += new Vector3(-x, 0, 0);
+    }
+}
---
+++ Assets/SphereScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localPosition += new Vector3(Time.deltaTime*8, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localPosition += new Vector3(-Time.deltaTime*8, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.localPosition += new Vector3(0, 0, Time.deltaTime*8);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.localPosition += new Vector3(0, 0, -Time.deltaTime*8);
+        }
+    }
+}
