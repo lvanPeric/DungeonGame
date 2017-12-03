938821c48f863332d76394fb37a9d57add3983ed
Ivan <ivan.peric93100@gmail.com>
2017-12-03 16:15:17 +0100

    import du dernier personnage, changement de vitesse des ennemis, texte de la salle 3 et timer

---
+++ Assets/ThirdRoomTextScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThirdRoomTextScript : MonoBehaviour {
+    private float timer = 0;
+    private Text t;
+
+    private void Start()
+    {
+        t = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (GameObject.FindGameObjectWithTag("Door2"))
+        {
+            t.text = null;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+            if (timer < 10f && timer > 0f)
+            {
+                t.text = "Maintenant que vous avez tué les chevaliers, il est temps de délivrer votre bien-aimée en la trouvant dans la forêt mais faites attention à la terreur de la forêt";
+            } else if (timer < 15f && timer > 10f)
+            {
+                t.text = "Vous avez 300 Secondes";
+            } else {
+                t.text = null;
+            }
+        }
+    }
+}
---
+++ Assets/TimerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    private float timer = 0;
+    private Text t;
+
+    // Use this for initialization
+    void Start()
+    {
+        t = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("Door2"))
+        {
+            timer = 0;
+            t.text = (int)timer + "";
+        }
+        else
+        {
+            timer += Time.deltaTime;
+            t.text = (int)timer + "";
+        }
+    }
+
+    public void Reset()
+    {
+        timer = 0;
+    }
+}
